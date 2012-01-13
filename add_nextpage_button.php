084f7dd143003fa00c827ab388e849bcfea9320c
Thomas Clausen <kontakt@thomasclausen.dk>
2012-01-13 15:49:33 +0100

    Initial commit

---
+++ add_nextpage_button.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: "Next page" button
+Description: Add "Next page" button to TinyMCE. REMEMBER to use the "wp_link_pages()" function to display the links.
+Version: 0.2
+License: GPLv2 or later
+Author: Thomas Clausen
+Author URI: http://www.thomasclausen.dk/wordpress/
+*/
+
+// Add "Next page" button to TinyMCE
+function add_next_page_button( $mce_buttons ) {
+	$pos = array_search( 'wp_more', $mce_buttons, true );
+	if ( $pos !== false ) {
+		$tmp_buttons = array_slice( $mce_buttons, 0, $pos+1 );
+		$tmp_buttons[] = 'wp_page';
+		$mce_buttons = array_merge( $tmp_buttons, array_slice( $mce_buttons, $pos+1 ) );
+	}
+	return $mce_buttons;
+}
+add_filter( 'mce_buttons', 'add_next_page_button' );
+
+// Customize the "wp_link_pages()" to be able to display both numbers and prev/next links
+function add_next_and_number( $args ) {
+	if ( $args['next_or_number'] == 'next_and_number' ) {
+		global $page, $numpages, $multipage, $more, $pagenow;
+		$args['next_or_number'] = 'number';
+		$prev = '';
+		$next = '';
+		if ( $multipage ) {
+			if ( $more ) {
+				$i = $page - 1;
+				if ( $i && $more ) {
+					$prev .= _wp_link_page($i);
+					$prev .= $args['link_before'] . $args['previouspagelink'] . $args['link_after'] . '</a>';
+				}
+				$i = $page + 1;
+				if ( $i <= $numpages && $more ) {
+					$next .= _wp_link_page($i);
+					$next .= $args['link_before'] . $args['nextpagelink'] . $args['link_after'] . '</a>';
+				}
+			}
+		}
+		$args['before'] = $args['before'] . $prev;
+		$args['after'] = $next . $args['after'];    
+	}
+	return $args;
+}
+add_filter( 'wp_link_pages_args', 'add_next_and_number' ); ?>
